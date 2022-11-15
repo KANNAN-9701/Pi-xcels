6d294dde62074cdde3655fe734526034944f3319
Glitch (starter-cra-and-express) <none>
2022-11-15 07:28:26 +0000

    🌈👫 Checkpoint ./src/App.js:51075855/4418 ./src/components/Movies.jsx:51075855/3844

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   const [movies, setMovies] = useState([]);
@@ -14,23 +13,12 @@ function App() {
   }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and your changes will live-update automatically.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>Nice Movies:</p>
-        <p>{JSON.stringify(movies)}</p>
-        
-      </header>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Movies} />
+          <Route path="/movie" component={MovieDetails} />
+        </Switch>
+      </Router>
     </div>
   );
 }
